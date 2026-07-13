c6f20f045f93bcb7f45f57fb6eb395196c52f36b
sodj <3197392880@qq.com>
2026-07-13 14:16:33 +0800

    接口限流：同一IP 1 分钟内最多 5 次

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,15 +1,15 @@
-// 登录/注册接口限流：同一IP 15 分钟内最多 10 次
+// 登录/注册接口限流：同一IP 1 分钟内最多 5 次
 const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 10,
+  windowMs: 1 * 60 * 1000,
+  max: 5,
   message: { code: 429, message: '请求过于频繁，请稍后再试' },
 });
 
 const registerLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 10,
+  windowMs: 1 * 60 * 1000,
+  max: 5,
   message: { code: 429, message: '请求过于频繁，请稍后再试' },
 });
 
